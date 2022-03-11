75bf2fdc3a030bd58448e65b0a3bdf4ad27e448e
jinyongp <dev.jinyongp@gmail.com>
2022-03-11 11:14:01 +0900

    Feat: serverless http 요청 추가

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
-};
+const data = require('./data.json');
 
-exports.event = (event, callback) => {
-  callback();
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+exports.http = (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else {
+    const pageNo = req?.query.pageNo || 1;
+    const perPage = req?.query.perPage || 10;
+    const start = perPage * (pageNo - 1);
+    const end = perPage + start;
+    res
+      .status(200)
+      .type('json')
+      .send(JSON.stringify(data.slice(start, end)));
+  }
 };
